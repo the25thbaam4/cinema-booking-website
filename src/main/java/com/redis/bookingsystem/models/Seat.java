fed950738fac2aa25c0d499d86c469833a000c39
Ace <the25thbaam@tutanota.com>
2023-07-28 00:43:45 +0200

    time to work on this project again

--- src/main/java/com/redis/bookingsystem/models/Seat.java
+++ src/main/java/com/redis/bookingsystem/models/Seat.java
@@ -2,7 +2,7 @@ package com.redis.bookingsystem.models;
 
 import jakarta.persistence.Entity;
 
-@Entity
+
 public class Seat {
     private Long id;
 }
--- src/main/java/com/redis/bookingsystem/repositories/SeatRepo.java
+++ src/main/java/com/redis/bookingsystem/repositories/SeatRepo.java
@@ -1,7 +1,5 @@
 package com.redis.bookingsystem.repositories;
 
-import com.redis.bookingsystem.models.Seat;
-import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface SeatRepo extends JpaRepository<Seat, > {
+public interface SeatRepo{
 }
