5fad51c5ca6b682e42c877356986c6b4aae10d41
Ace <the25thbaam@tutanota.com>
2023-09-18 21:28:46 +0200

    Frontend: added a home page with sliding pictures with some CSS formating and JavaScript func

--- src/main/java/com/redis/bookingsystem/controller/IndexController.java
+++ src/main/java/com/redis/bookingsystem/controller/IndexController.java
@@ -1,6 +1,7 @@
 package com.redis.bookingsystem.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -11,5 +12,10 @@ public class IndexController {
     @GetMapping("/")
     public ModelAndView index(){
         return new ModelAndView("index");
+    }
+
+    @GetMapping("/movie-page")
+    public ModelAndView movies(){
+        return new ModelAndView("movie-page");
     }
 }
